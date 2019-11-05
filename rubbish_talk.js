0b68ffa858314e0c6a736b5a1d4cd064e7a4ab24
Wendy <wendysu03@gmail.com>
2019-11-06 01:27:53 +0800

    trash_talk.v1

--- rubbish_talk.js
+++ rubbish_talk.js
@@ -10,7 +10,7 @@ const phrase = ['很簡單', '很容易', '很快', '很正常']
 
 function rubbishTalk(job) {
   const person = career.results.find(person => person.name_en === job)
-  return job ? `身為一個${person.name}，${randomIndex(person.task)}，應該${randomIndex(phrase)}!` : '請選擇一個'
+  return job ? `身為一個${person.name}，${randomIndex(person.task)}，應該${randomIndex(phrase)}吧!` : '請選擇一個'
 }
 
 module.exports = rubbishTalk
